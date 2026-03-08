f50b8df20f39f38a2ae33b79983092ae420e4b44
Hoang Nguyen <uchiha.kamui2004@gmail.com>
2026-03-07 18:06:43 -0800

    Testing API Call

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from auth import router as auth_router
 from course import router as course_router
 from ml.ml_router import router as ml_router
 from schedules import router as schedules_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="major_map backend")
 
@@ -12,6 +13,15 @@ app.include_router(auth_router)
 app.include_router(course_router)
 app.include_router(schedules_router)
 app.include_router(ml_router)
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
